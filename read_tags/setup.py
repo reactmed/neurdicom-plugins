8e8c2b5d25275540cb661e26fc0aba7e2a89c17e
Roman Baygildin <rbaygildin95@gmail.com>
2018-04-13 19:00:53 +0300

    Init commit

--- read_tags/setup.py
+++ read_tags/setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 import os.path as path
-import read_tags
 
 setup(
     name='read_tags',
-    version=read_tags.__version__,
+    version='1.0',
+    author='Roman Baygild',
+    author_email='egdeveloper@mail.ru',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Medical images processing :: Processing',
+    ],
+    url='http://github.com/reactmed/neurdicom-plugins',
+    license='MIT',
+    keywords='DICOM',
     packages=find_packages(),
     install_requires=[
         'pydicom'
