baddd2c2096c04d7d0555a2076c5da51b3445e90
Roman Baygildin <rbaygildin95@gmail.com>
2018-04-17 04:47:20 +0300

    Updated FCM

--- fcm/fcm/plugin.py
+++ fcm/fcm/plugin.py
@@ -2,6 +2,7 @@ from skfuzzy.cluster import cmeans
 from pydicom import Dataset
 import numpy as np
 import cv2 as cv
+from dipy.segment.mask import median_otsu
 
 
 class Plugin:
@@ -9,9 +10,16 @@ class Plugin:
     def initialize(self):
         pass
 
-    def process(self, img, n_clusters=3, m=2, eps=0.01, max_it=1000, **kwargs):
+    def process(self, img, **kwargs):
+        n_clusters = kwargs.get('n_clusters', 3)
+        m = kwargs.get('m', 2)
+        eps = kwargs.get('eps', 0.01)
+        max_it = kwargs.get('max_it', 100)
+        numpass = kwargs.get('numpass', 5)
+        median_radius = kwargs.get('median_radius', 10)
         if isinstance(img, Dataset):
             img = img.pixel_array
+        img, _ = median_otsu(img, numpass=numpass, median_radius=median_radius)
         flat = img.reshape((1, -1))
         c, u, a1, a2, a3, a4, a5 = cmeans(flat, n_clusters, m, eps, max_it)
         tumor_index = np.argmax(c, axis=0)
