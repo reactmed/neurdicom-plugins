f959c5895cde63dc1abf5f0bf0ff38348f46b568
Roman Baygildin <rbaygildin95@gmail.com>
2018-05-16 07:57:41 +0300

    Updated (added libraries)

---
+++ kernel_kmeans/kernel_kmeans/extension/src/CMakeFiles/kernel_kmeans.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kernel_kmeans.dir/library.cpp.o"
+  "CMakeFiles/kernel_kmeans.dir/Plugin.cpp.o"
+  "libkernel_kmeans.pdb"
+  "libkernel_kmeans.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kernel_kmeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
