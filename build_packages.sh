a99d372fbfb2199b81a1110eea38add59eb987a0
Roman Baygildin <rbaygildin95@gmail.com>
2018-06-08 04:29:58 +0300

    Building and uploading packages scripts

---
+++ build_packages.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+OS="$(uname)"
+
+logInfo(){
+  echo -e "${CYAN}`date "+%Y-%m-%d %H:%M:%S"`${NC} ${BLUE}[INFO]${NC}: $1"
+}
+
+logInfoStep(){
+  echo -e "${CYAN}`date "+%Y-%m-%d %H:%M:%S"`${NC} ${BLUE}[INFO]${NC}: $1 ${GREEN}[OK]${NC}"
+}
+
+logErr(){
+  echo -e "${CYAN}`date "+%Y-%m-%d %H:%M:%S"`${NC} ${RED}[ERR]${NC}: $1"
+}
+
+logWarn(){
+  echo -e "${CYAN}`date "+%Y-%m-%d %H:%M:%S"`${NC} ${YELLOW}[WARN]${NC}: $1"
+}
+
+if [[ $# -lt 1 ]]; then
+    logErr "Please specify packages to build"
+    exit -1
+fi
+
+while [[ $# -gt 0 ]]
+do
+    logInfo "Building package '$1'"
+    cd "$1"
+    python3 setup.py sdist bdist_wheel
+    cd ..
+    shift
+done
